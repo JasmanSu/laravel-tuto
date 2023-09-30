761927485bd150bdfeb6ef24bdf841ea53e388e1
Jasman Su <suiqqwan@gmail.com>
2023-09-30 10:12:57 +0800

    First Commit for tutorial laravel

---
+++ resources/views/post.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed ipsum sit amet neque semper aliquet eu non lectus. Nullam gravida semper nunc eget malesuada. Nam auctor magna ac sapien lobortis volutpat. Fusce ac leo ac magna fringilla tempor a non ligula. Etiam at tincidunt eros, congue laoreet nisl. In ex velit, fringilla ut pretium ac, pellentesque non libero.
+        </p>
+    </article>
+
+    <a href="/"> Go Back</a>
+
+   
+</body>
+
+
+
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+
+<title>My Blog</title>
+<link rel="stylesheet" href="app.css">
+
+
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed ipsum sit amet neque semper aliquet eu non lectus. Nullam gravida semper nunc eget malesuada. Nam auctor magna ac sapien lobortis volutpat. Fusce ac leo ac magna fringilla tempor a non ligula. Etiam at tincidunt eros, congue laoreet nisl. In ex velit, fringilla ut pretium ac, pellentesque non libero.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Second Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed ipsum sit amet neque semper aliquet eu non lectus. Nullam gravida semper nunc eget malesuada. Nam auctor magna ac sapien lobortis volutpat. Fusce ac leo ac magna fringilla tempor a non ligula. Etiam at tincidunt eros, congue laoreet nisl. In ex velit, fringilla ut pretium ac, pellentesque non libero.
+        </p>
+    </article>
+
+    <article>
+        <h1><a href="/post">My Third Post</a></h1>
+
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sed ipsum sit amet neque semper aliquet eu non lectus. Nullam gravida semper nunc eget malesuada. Nam auctor magna ac sapien lobortis volutpat. Fusce ac leo ac magna fringilla tempor a non ligula. Etiam at tincidunt eros, congue laoreet nisl. In ex velit, fringilla ut pretium ac, pellentesque non libero.
+        </p>
+    </article>
+</body>
+
+
+
